7eaa9d6e0e6fa899b5d6a7a81a7a0e3a44a77a94
JonathanPegaz <pegazjonathan@gmail.com>
2022-12-28 01:14:49 +0100

    Initial Commit

---
+++ symbiose_act3/symbiose_act3App.swift
@@ -0,0 +1,17 @@
+//
+//  symbiose_act3App.swift
+//  symbiose_act3
+//
+//  Created by Jonathan Pegaz on 28/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct symbiose_act3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
